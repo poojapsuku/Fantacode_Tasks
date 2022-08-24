9ef404ad35b47d584cb6a8177484b32c33472029
poojapsuku <poojapsuku@gmail.com>
2022-08-24 14:24:48 +0530

    Task1 Commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("\nThe Current Day is : " + DateTime.Now.ToString("dddd"));
+            Console.WriteLine("The Current Month is : "+DateTime.Now.ToString("MMMM"));
+
+        }
+    }
+}
